ad1cdd1b9a06db070406601d9ffb510c49c6d5af
Chiicake <pankaiwen1231@outlook.com>
2025-09-29 14:36:43 +0800

    smart contract abi

---
+++ example/smart_contracts/smart_contracts_abi/store.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.30;
+
+// solcjs --abi --bin .\store.sol -o build
+contract Store {
+    event ItemSet(bytes32 key, bytes32 value);
+
+    string public version;
+    mapping (bytes32 => bytes32) public items;
+
+    constructor(string memory _version) public {
+        version = _version;
+    }
+
+    function setItem(bytes32 key, bytes32 value) external {
+        items[key] = value;
+        emit ItemSet(key, value);
+    }
+}
